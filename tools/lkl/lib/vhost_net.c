a2db310ab506bc5e3dd0297788f912e810b6b561
Ryo Nakamura <upa@haeena.net>
2020-09-04 02:17:49 +0900

    ato chotto!

--- tools/lkl/lib/vhost_net.c
+++ tools/lkl/lib/vhost_net.c
@@ -1,14 +1,18 @@
+#include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <lkl_host.h>
+#include <stdlib.h>
 #include <sys/eventfd.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
-#include <poll.h>
+#include <sys/poll.h>
+#include <sys/epoll.h>
+#include <linux/if.h>
 #include <linux/if_tun.h>
 #include <linux/vhost.h>
 
+#include <lkl_host.h>
 #include "virtio.h"
 
 
@@ -67,7 +71,7 @@
 		lkl_host_ops.panic();					\
 	} while (0)
 
-
+#define VHOST_TRANSPORT_F_MASK	0x3FF0000000
 
 struct vhost_net_dev {
 	struct virtio_dev dev;
@@ -77,20 +81,27 @@ struct vhost_net_dev {
 	int backend_fd;
 	int vnet_hdr_len;
 
+	uint64_t vhost_net_features;
+
 	/* used for poll thread */
 	lkl_thread_t poll_tid;
 	int qnum;
 	int pollstop;
 };
 
-static int vhost_net_ioctl(struct vhost_net_dev *dev, unsigned int long req,
-			   void *arg)
+
+#define vhost_net_ioctl(d, r, a) \
+	_vhost_net_ioctl(d, r, a, #r)
+static int _vhost_net_ioctl(struct vhost_net_dev *dev, unsigned int long req,
+			    void *arg, char *reqstr)
 {
 	int ret;
 
+	lkl_printf("%s\n", reqstr);
+
 	ret = ioctl(dev->vhost_net_fd, req, arg);
 	if (ret < 0) {
-		lkl_printf("%s: ioctl %lu: %s\n", __func__, req,
+		lkl_printf("%s: ioctl %s: %s\n", __func__, reqstr,
 			   strerror(errno));
 	}
 
@@ -123,7 +134,7 @@ static int vhost_net_read(void *data, int offset, void *res, int size)
 {
 	struct virtio_dev *vdev = (struct virtio_dev *)data;
 	struct vhost_net_dev *dev = netdev_of(vdev);
-	uint32_t val;
+	uint32_t val = 0;
 
 	if (offset >= VIRTIO_MMIO_CONFIG) {
 		offset -= VIRTIO_MMIO_CONFIG;
@@ -163,6 +174,7 @@ static int vhost_net_read(void *data, int offset, void *res, int size)
 		break;
 	case VIRTIO_MMIO_STATUS:
 		val = vdev->status;
+		lkl_printf("%s: status is 0x%x\n", __func__, vdev->status);
 		break;
 	case VIRTIO_MMIO_CONFIG_GENERATION:
 		val = vdev->config_gen;
@@ -173,11 +185,13 @@ static int vhost_net_read(void *data, int offset, void *res, int size)
 
 	*(uint32_t *)res = htole32(val);
 
+        lkl_printf("%s: offset is %x val is %d\n", __func__, offset, val);
+
 	return 0;
 }
 
-static inline void vhost_net_write_driver_features(struct vhost_net_dev *dev,
-						   uint32_t val)
+static int vhost_net_write_driver_features(struct vhost_net_dev *dev,
+					   uint32_t val)
 {
 	uint64_t tmp;
 
@@ -188,6 +202,9 @@ static inline void vhost_net_write_driver_features(struct vhost_net_dev *dev,
 		tmp = dev->dev.driver_features & 0xFFFFFFFF00000000;
 		dev->dev.driver_features = tmp | val;
 	}
+
+	return vhost_net_ioctl(dev, VHOST_SET_FEATURES,
+			       &dev->dev.driver_features);
 }
 
 static int vhost_net_set_vring_num(struct vhost_net_dev *dev, uint32_t num)
@@ -210,6 +227,7 @@ static int vhost_net_set_eventfd(struct vhost_net_dev *dev)
 		lkl_printf("%s: eventfd(): %s\n", __func__, strerror(errno));
 		return -1;
 	}
+
 	q->call_fd = eventfd(0, 0);
 	if (q->call_fd < 0) {
 		lkl_printf("%s: eventfd(): %s\n", __func__, strerror(errno));
@@ -218,17 +236,16 @@ static int vhost_net_set_eventfd(struct vhost_net_dev *dev)
 
 	f.index = vdev->queue_sel;
 	f.fd = q->kick_fd;
-	if (vhost_net_ioctl(dev, VHOST_SET_VRING_KICK, &f) < 0) {
-		lkl_printf("VHOST_SET_VRING_KICK failed\n");
+	if (vhost_net_ioctl(dev, VHOST_SET_VRING_KICK, &f) < 0)
 		return -1;
-	}
 
 	f.index = vdev->queue_sel;
 	f.fd = q->call_fd;
-	if (vhost_net_ioctl(dev, VHOST_SET_VRING_CALL, &f) < 0) {
-		lkl_printf("VHOST_SET_VRING_CALL failed\n");
+	if (vhost_net_ioctl(dev, VHOST_SET_VRING_CALL, &f) < 0)
 		return -1;
-	}
+
+	lkl_printf("q=%d kick=%d call=%d\n",
+		   vdev->queue_sel, q->kick_fd, q->call_fd);
 
 	return 0;
 }
@@ -239,6 +256,10 @@ static int vhost_net_kick(struct vhost_net_dev *dev, uint32_t qidx)
 	uint64_t v = 1;
 	int ret;
 
+	lkl_printf("%s: q %u avail index %u\n",
+		   __func__, qidx, q->avail->idx);
+
+	__sync_synchronize();
 	ret = write(q->kick_fd, &v, sizeof(v));
 	if (ret < 0)
 		lkl_printf("%s: write(): %s\n", __func__, strerror(errno));
@@ -259,6 +280,11 @@ static int vhost_net_set_vring_addr(struct vhost_net_dev *dev)
 	a.avail_user_addr = (uintptr_t)q->avail;
 	a.log_guest_addr = (uintptr_t)q->log;
 
+	lkl_printf("idx %u\n", a.index);
+	lkl_printf("desc_user %llx\n", a.desc_user_addr);
+	lkl_printf("used_user %llx\n", a.used_user_addr);
+	lkl_printf("avail_user %llx\n", a.avail_user_addr);
+
 	if (vhost_net_ioctl(dev, VHOST_SET_VRING_ADDR, &a) < 0) {
 		lkl_printf("%s: ioctl VHOST_SET_VRING_ADDR failed: %s\n",
 			   __func__, strerror(errno));
@@ -276,6 +302,8 @@ static int vhost_net_set_backend(struct vhost_net_dev *dev)
 		.fd	= dev->backend_fd,
 	};
 
+	lkl_printf("set backend q %d fd %d\n", f.index, f.fd);
+
 	ret = vhost_net_ioctl(dev, VHOST_NET_SET_BACKEND, &f);
 	if (ret < 0)
 		lkl_printf("ioctl VHOSET_NET_SET_BACKEND failed: %s\n",
@@ -292,10 +320,16 @@ static int vhost_net_set_mem_table(struct vhost_net_dev *dev)
 	} vmem;
 	int ret;
 
+	memset(&vmem, 0, sizeof(vmem));
+
 	vmem.m.nregions = 1;
 	vmem.r[0].guest_phys_addr = lkl_host_ops.memory_start;
 	vmem.r[0].memory_size = lkl_host_ops.memory_size;
 	vmem.r[0].userspace_addr = lkl_host_ops.memory_start;
+
+	lkl_printf("guest %lx\n", vmem.r[0].guest_phys_addr);
+	lkl_printf("size  %ld\n", vmem.r[0].memory_size);
+	lkl_printf("user  %lx\n", vmem.r[0].userspace_addr);
 
 	ret = vhost_net_ioctl(dev, VHOST_SET_MEM_TABLE, &vmem);
 	if (ret < 0)
@@ -303,6 +337,16 @@ static int vhost_net_set_mem_table(struct vhost_net_dev *dev)
 			   strerror(errno));
 
 	return ret;
+}
+
+static int vhost_net_set_vring_base(struct vhost_net_dev *dev)
+{
+	struct vhost_vring_state s;
+
+	s.index = dev->dev.queue_sel;
+	s.num = 0;	/* XXX */
+
+	return vhost_net_ioctl(dev, VHOST_SET_VRING_BASE, &s);
 }
 
 static inline void set_ptr_low(void **ptr, uint32_t val)
@@ -321,14 +365,28 @@ static inline void set_ptr_high(void **ptr, uint32_t val)
 	*ptr = (void *)(long)tmp;
 }
 
-static inline void set_status(struct virtio_dev *dev, uint32_t val)
+static inline int set_status(struct virtio_dev *vdev, uint32_t val)
 {
-	if ((val & LKL_VIRTIO_CONFIG_S_FEATURES_OK) &&
-	    (!(dev->driver_features & BIT(LKL_VIRTIO_F_VERSION_1)) ||
-	     !(dev->driver_features & BIT(LKL_VIRTIO_RING_F_EVENT_IDX)) ||
-	     !(dev->driver_features == dev->device_features)))
-		val &= ~LKL_VIRTIO_CONFIG_S_FEATURES_OK;
-	dev->status = val;
+	struct vhost_net_dev *dev = netdev_of(vdev);
+
+	printf("%s: val 0x%x\n", __func__, val);
+	printf("%s: driver 0x%016lx\n", __func__, vdev->driver_features);
+	printf("%s: device 0x%016lx\n", __func__, vdev->device_features);
+
+	if (val & LKL_VIRTIO_CONFIG_S_FEATURES_OK) {
+
+		if ((vdev->device_features & vdev->driver_features) == 
+		    vdev->device_features) {
+			if (vhost_net_ioctl(dev, VHOST_SET_FEATURES,
+					    &vdev->driver_features) < 0)
+				return -1;
+			vdev->status = val;
+		}
+ 	}
+
+	vdev->status = val;
+
+	return 0;
 }
 
 static int vhost_net_write(void *data, int offset, void *res, int size)
@@ -352,6 +410,8 @@ static int vhost_net_write(void *data, int offset, void *res, int size)
 		return -LKL_EINVAL;
 
 	val = le32toh(*(uint32_t *)res);
+	lkl_printf("%s: q %d offset is %x val is %d\n",
+		   __func__, vdev->queue_sel, offset, val);
 
 	switch (offset) {
 	case VIRTIO_MMIO_DEVICE_FEATURES_SEL:
@@ -365,7 +425,7 @@ static int vhost_net_write(void *data, int offset, void *res, int size)
 		vdev->driver_features_sel  = val;
 		break;
 	case VIRTIO_MMIO_DRIVER_FEATURES:
-		vhost_net_write_driver_features(dev, val);
+		ret = vhost_net_write_driver_features(dev, val);
 		break;
 	case VIRTIO_MMIO_QUEUE_SEL:
 		vdev->queue_sel = val;
@@ -375,17 +435,20 @@ static int vhost_net_write(void *data, int offset, void *res, int size)
 		ret = vhost_net_set_vring_num(dev, val);
 		if (ret < 0)
 			break;
-		ret = vhost_net_set_eventfd(dev);
 		q->log = lkl_host_ops.mem_alloc(4096);	/* XXX: ?????? */
+		ret = vhost_net_set_eventfd(dev);
 		dev->qnum++;
 		break;
 	case VIRTIO_MMIO_QUEUE_READY:
 		vdev->queue[vdev->queue_sel].ready = val;
 		if (val) {
-			ret = vhost_net_set_backend(dev);
+			ret = vhost_net_set_mem_table(dev);
 			if (ret < 0)
 				break;
-			ret = vhost_net_set_mem_table(dev);
+			ret = vhost_net_set_vring_base(dev);
+			if (ret < 0)
+				break;
+			ret = vhost_net_set_backend(dev);
 			if (ret < 0)
 				break;
 		}
@@ -397,8 +460,8 @@ static int vhost_net_write(void *data, int offset, void *res, int size)
 		vdev->int_status = 0;
 		break;
 	case VIRTIO_MMIO_STATUS:
-		set_status(vdev, val);
-		break;
+		ret = set_status(vdev, val);
+ 		break;
 	case VIRTIO_MMIO_QUEUE_DESC_LOW:
 		set_ptr_low((void **)&q->desc, val);
 		break;
@@ -446,6 +509,8 @@ static struct vhost vhost_net = {
 
 static void virtio_deliver_irq(struct virtio_dev *dev)
 {
+	lkl_printf("DOOOO IRQ to %d\n", dev->irq);
+
 	dev->int_status |= VIRTIO_MMIO_INT_VRING;
 	__sync_synchronize();
 	lkl_trigger_irq(dev->irq);
@@ -457,67 +522,56 @@ static void vhost_net_poll_thread(void *arg)
 	struct virtio_queue *q;
 	struct pollfd x[NUM_QUEUES];
 	uint64_t val;
-	int qnum = 0;
 	int ret, n;
+	int qnum = 0;
 
 	/* unlike virtio_net.c this thread polls call_fd of
 	 * virtqueues, and then invokes irq */
 
 	do {
-		if (qnum != dev->qnum) {
-			qnum = dev->qnum;
-			for (n = 0; n < qnum; n++) {
-				q = &dev->dev.queue[n];
-				x[n].fd = q->call_fd;
-				x[n].events = POLLIN;
-			}
-		}
-
-		if (qnum == 0) {
-			usleep(10);	/* no queues enabled */
-			continue;
-		}
-
 		if (dev->pollstop)
 			break;
-
-		if (poll(x, qnum, 10) < 0) {
-			lkl_printf("%s: poll failed: %s\n", __func__,
-				   strerror(errno));
+		if (qnum != dev->qnum) {
+			qnum = dev->qnum;
+			for (n = 0; n < NUM_QUEUES; n++) {
+				q = &dev->dev.queue[n];
+				if (q->call_fd) {
+					x[n].fd = q->call_fd;
+					x[n].events = POLLIN;
+				}
+			}
 		}
 
+		printf("%s: qnum %d\n", __func__, qnum);
+
+		do {
+			ret = poll(x, qnum, 1000);
+		} while (ret == -1 && errno == EINTR);
+
+		if (ret < 0)
+			perror("vhost_net poll");
+
 		for (n = 0; n < qnum; n++) {
+
 			if (!x[n].revents & POLLIN)
 				continue;
-			else {
-				ret = read(x[n].fd, &val, sizeof(val));
-				if (ret < 0) {
-					lkl_printf("%s: read failed: %s\n",
-						   __func__, strerror(errno));
-					continue;
-				}
-				virtio_deliver_irq(&dev->dev);
+
+			ret = read(x[n].fd, &val, sizeof(val));
+			if (ret < 0) {
+				lkl_printf("%s: read failed: %s\n",
+					   __func__, strerror(errno));
+				continue;
 			}
+			lkl_printf("%s: val is %lu\n", __func__, val);
+
+			virtio_deliver_irq(&dev->dev);
 		}
 
 	} while (1);
 }
 
-static int probe_vnet_hdr_len(int fd)
-{
-	int val;
-
-	if (ioctl(fd, TUNGETVNETHDRSZ, &val) < 0) {
-		lkl_printf("ioctl TUNGETVNETHDRSZ failed: %s\n",
-			   strerror(errno));
-		return -1;
-	}
-
-	return val;
-}
-
 struct vhost_net_dev *registered_devs[MAX_NET_DEVS];
-static int registered_dev_idx = 1;
+static int registered_dev_idx = 0;
 
 static int dev_register(struct vhost_net_dev *dev)
 {
@@ -531,11 +585,21 @@ static int dev_register(struct vhost_net_dev *dev)
 	}
 }
 
-int lkl_vhost_net_add(int backend_fd, struct lkl_netdev_args *args)
+int lkl_vhost_net_add(char *path, struct lkl_netdev_args *args)
 {
 	struct vhost_net_dev *dev;
-	int ret = -LKL_ENOMEM;
-	uint64_t features;
+	struct ifreq ifr;
+	int backend_fd, ret, tap_arg = 0;
+	int vnet_hdr_sz = sizeof(struct lkl_virtio_net_hdr_v1) ;
+	int offload = args->offload;
+
+	backend_fd = open(path, O_RDWR);
+	if (backend_fd < 0) {
+		lkl_printf("failed to open %s: %s\n", path, strerror(errno));
+		return -1;
+	}
+
+	ret = -LKL_ENOMEM;
 
 	dev = lkl_host_ops.mem_alloc(sizeof(*dev));
 	if (!dev)
@@ -545,17 +609,19 @@ int lkl_vhost_net_add(int backend_fd, struct lkl_netdev_args *args)
 	dev->dev.device_id = LKL_VIRTIO_ID_NET;
 	if (args) {
 		if (args->mac) {
-			dev->dev.device_features |= BIT(LKL_VIRTIO_NET_F_MAC);
+			/* do not set LKL_VIRTIO_NET_F_MAC to
+			 * dev->dev.device_feautures because it is not
+			 * supported on vhost_net */
 			memcpy(dev->config.mac, args->mac, LKL_ETH_ALEN);
 		}
-		dev->dev.device_features |= args->offload;
+		//dev->dev.device_features |= offload;
 	}
 	dev->dev.config_data = &dev->config;
 	dev->dev.config_len = sizeof(dev->config);
 	dev->backend_fd = backend_fd;
 
 	if ((dev->vhost_net_fd = open("/dev/vhost-net", O_RDWR)) < 0) {
-		lkl_printf("failed to open /dev/vhost-net: %d\n",
+		lkl_printf("failed to open /dev/vhost-net: %s\n",
 			   strerror(errno));
 		ret = dev->vhost_net_fd;
 		goto out_free;
@@ -566,32 +632,62 @@ int lkl_vhost_net_add(int backend_fd, struct lkl_netdev_args *args)
 		goto out_free;
 	}
 
-	if (vhost_net_ioctl(dev, VHOST_GET_FEATURES, &features) < 0) {
-		lkl_printf("ioctl VHOST_GET_FEATURES failed: %s\n",
-			   strerror(errno));
-		goto out_free;
+	memset(&ifr, 0, sizeof(ifr));
+	ifr.ifr_flags = IFF_TAP | IFF_NO_PI | IFF_VNET_HDR;
+	ret = ioctl(backend_fd, TUNSETIFF, &ifr);
+	if (ret < 0) {
+		fprintf(stderr, "%s: failed to attach to: %s\n",
+			path, strerror(errno));
+		goto out_close;
 	}
-	dev->dev.device_features |= features;
 
-	if ((dev->vnet_hdr_len = probe_vnet_hdr_len(dev->backend_fd)) < -1)
-		goto out_free;
 
-	if (dev->vnet_hdr_len == 0) {
-		/* backend does not supports vnet hdr. drop affected
-		 * bits from features (I saw qemu/hw/net/virtio-net.c,
-		 * virtio_net_get_features).
-		 */
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_CSUM);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_HOST_TSO4);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_HOST_TSO6);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_HOST_ECN);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_GUEST_CSUM);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_GUEST_TSO4);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_GUEST_TSO6);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_GUEST_ECN);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_GUEST_UFO);
-		dev->dev.device_features &= ~BIT(LKL_VIRTIO_NET_F_HOST_UFO);
+
+	printf("offload is %x\n", offload);
+	if (offload & (BIT(LKL_VIRTIO_NET_F_MRG_RXBUF)))
+		printf("mrg rx buf!\n");
+	else
+		printf("No mrg rx buf!\n");
+	if (offload & BIT(LKL_VIRTIO_NET_F_GUEST_CSUM))
+		tap_arg |= TUN_F_CSUM;
+	if (offload & (BIT(LKL_VIRTIO_NET_F_GUEST_TSO4) |
+		       BIT(LKL_VIRTIO_NET_F_MRG_RXBUF))) {
+		tap_arg |= TUN_F_TSO4 | TUN_F_CSUM;
+		printf("TSO4!\n");
 	}
+	if (offload & (BIT(LKL_VIRTIO_NET_F_GUEST_TSO6)))
+		tap_arg |= TUN_F_TSO6 | TUN_F_CSUM;
+
+	if (ioctl(backend_fd, TUNSETVNETHDRSZ, &vnet_hdr_sz) != 0) {
+		fprintf(stderr, "%s: failed to TUNSETVNETHDRSZ to: %s\n",
+			path, strerror(errno));
+		goto out_close;
+	}
+	if (ioctl(backend_fd, TUNSETOFFLOAD, tap_arg) != 0) {
+		fprintf(stderr, "%s: failed to TUNSETOFFLOAD: %s\n",
+			path, strerror(errno));
+		goto out_close;
+	}
+
+
+
+	/* drop features unsupported by vhost */
+	uint64_t f;
+	vhost_net_ioctl(dev, VHOST_GET_FEATURES, &f);
+	/*
+	vf &= ~BIT(VIRTIO_F_NOTIFY_ON_EMPTY);
+	vf &= ~BIT(VIRTIO_RING_F_INDIRECT_DESC);
+	vf &= ~BIT(VIRTIO_RING_F_EVENT_IDX);
+	vf &= ~BIT(VHOST_F_LOG_ALL);
+	vf &= ~BIT(VIRTIO_F_ANY_LAYOUT);
+	vf &= ~BIT(VIRTIO_F_VERSION_1);
+	vf &= ~BIT(VHOST_NET_F_VIRTIO_NET_HDR);
+	vf &= ~BIT(VIRTIO_NET_F_MRG_RXBUF);
+	*/
+	f &= ~BIT(VIRTIO_F_IOMMU_PLATFORM);
+
+	dev->dev.device_features = f;
+	printf("device feature is 0x%lx\n", dev->dev.device_features);
 
 	dev->dev.vhost = &vhost_net;
 
@@ -648,4 +744,18 @@ void lkl_vhost_net_remove(int id)
 
 	virtio_dev_cleanup(&dev->dev);
 	lkl_host_ops.mem_free(dev);
+}
+
+struct lkl_netdev *lkl_vhost_net_create(void)
+{
+	struct lkl_netdev *nd;
+
+	nd = malloc(sizeof(*nd));
+	if (!nd) {
+		lkl_printf("%s: vhost-net: failed to allocate memory\n",
+			   __func__);
+		return NULL;
+	}
+	memset(nd, 0, sizeof(*nd));
+	return nd;
 }
